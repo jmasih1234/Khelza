aef92a01dcac8d6ea1cf310c9aeae84534756eb2
Joshua Masih <joshuafilm44@gmail.com>
2026-09-05 21:06:08 -0600

    Initial Commit

---
+++ Khel/KhelApp.swift
@@ -0,0 +1,17 @@
+//
+//  KhelApp.swift
+//  Khel
+//
+//  Created by Joshua Masih on 9/5/26.
+//
+
+import SwiftUI
+
+@main
+struct KhelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
